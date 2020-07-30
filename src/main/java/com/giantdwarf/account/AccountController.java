a9929693e8ac5df6ad1c70bbc1ac68c28e26e8e3
seungin yang <rhfpdk92@naver.com>
2020-07-30 21:58:13 +0900

    add SignUp & InitBinder

--- src/main/java/com/giantdwarf/account/AccountController.java
+++ src/main/java/com/giantdwarf/account/AccountController.java
@@ -1,16 +1,40 @@
 package com.giantdwarf.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
 
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm") // SignUpForm 데이터를 받을떄 바인딩해줌.
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
     @GetMapping("/sign-up")
-    public String signUpForm(Model model){
+    public String signUpForm(Model model) {
 //        model.addAttribute("signUpForm", new SignUpForm());
         model.addAttribute(new SignUpForm()); //class 이름과 동일한 attribute를 사용하면 생략 가능
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        return "redirect:/";
     }
 }
