8a673310d4f7d4ada53bdd8c722bf68b0d005187
seungin yang <rhfpdk92@naver.com>
2020-08-03 01:26:26 +0900

    add profile view

--- src/main/java/com/giantdwarf/account/AccountController.java
+++ src/main/java/com/giantdwarf/account/AccountController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -82,6 +83,17 @@ public class AccountController {
         accountService.sendSignUpConfirmEmail(account);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/profile/{nickname}")
+    public String viewProfile(@PathVariable String nickname, Model model, @CurrentUser Account account) {
+        Account byNickname = accountRepository.findByNickname(nickname);
+        if (Objects.isNull(nickname)) {
+            throw new IllegalArgumentException(nickname + "에 해당하는 사용자가 없습니다.");
+        }
+        model.addAttribute(byNickname); //이름을 주지 않으면 객체 타입의 이름이 들어감.
+        model.addAttribute("isOwner", byNickname.equals(account));
+        return "account/profile";
     }
 
 }
