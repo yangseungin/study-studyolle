ee9a7e903111285177ff67117e517e1e25cdc5d9
seungin yang <rhfpdk92@naver.com>
2020-08-14 11:19:30 +0900

    add new event view

---
+++ src/main/java/com/giantdwarf/event/EventController.java
@@ -0,0 +1,30 @@
+package com.giantdwarf.event;
+
+import com.giantdwarf.account.CurrentUser;
+import com.giantdwarf.domain.Account;
+import com.giantdwarf.domain.Study;
+import com.giantdwarf.event.form.EventForm;
+import com.giantdwarf.study.StudyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/study/{path}")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final StudyService studyService;
+
+    @GetMapping("/newEvent")
+    public String newEventForm(@CurrentUser Account account, @PathVariable String path, Model model){
+        Study study = studyService.getStudyToUpdateStatus(account, path);
+        model.addAttribute(account);
+        model.addAttribute(study);
+        model.addAttribute(new EventForm());
+        return "event/form";
+    }
+}
