5fd58a80f27e8dc5faf493738b6b2912d0a4e218
seungin yang <rhfpdk92@naver.com>
2020-08-16 19:32:05 +0900

    add Event edit

--- src/main/java/com/giantdwarf/domain/Event.java
+++ src/main/java/com/giantdwarf/domain/Event.java
@@ -90,5 +90,8 @@ public class Event {
         return false;
     }
 
+    public long getNumberOfAcceptedEnrollments() {
+        return this.enrollments.stream().filter(Enrollment::isAccepted).count();
+    }
 
 }
--- src/main/java/com/giantdwarf/event/EventService.java
+++ src/main/java/com/giantdwarf/event/EventService.java
@@ -3,7 +3,9 @@ package com.giantdwarf.event;
 import com.giantdwarf.domain.Account;
 import com.giantdwarf.domain.Event;
 import com.giantdwarf.domain.Study;
+import com.giantdwarf.event.form.EventForm;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,12 +17,17 @@ import java.time.LocalDateTime;
 public class EventService {
 
     private final EventRepository eventRepository;
-
+    private final ModelMapper modelMapper;
 
     public Event createEvent(Event event, Study study, Account account) {
         event.setCreatedBy(account);
         event.setCreateDateTime(LocalDateTime.now());
         event.setStudy(study);
         return eventRepository.save(event);
+    }
+
+    public void updateEvent(Event event, EventForm eventForm) {
+        modelMapper.map(eventForm,event);
+        //TODO 선착순 모임은 추가인원을 자동으로 수락으로 올려줄것
     }
 }
