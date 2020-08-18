f3919b663f98e6c57921f0f7b421e78080564dbc
seungin yang <rhfpdk92@naver.com>
2020-08-19 01:34:41 +0900

    add Notification domain

---
+++ src/main/java/com/giantdwarf/modules/notification/Notification.java
@@ -0,0 +1,36 @@
+package com.giantdwarf.modules.notification;
+
+import com.giantdwarf.modules.account.Account;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Notification {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String message;
+
+    private boolean checked;
+
+    @ManyToOne
+    private Account account;
+
+    private LocalDateTime createdLocalDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+
+}
---
+++ src/main/java/com/giantdwarf/modules/notification/NotificationType.java
@@ -0,0 +1,5 @@
+package com.giantdwarf.modules.notification;
+
+public enum NotificationType {
+    STUDY_CREATED, STUDY_UPDATED, EVENT_ENROLLMENT;
+}
