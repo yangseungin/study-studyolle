062a20a78086d28e6e796df11db4679fd2424ddb
seungin yang <rhfpdk92@naver.com>
2020-08-23 01:17:34 +0900

    replace member count

--- src/main/java/com/giantdwarf/modules/study/StudyRepositoryExtensionImpl.java
+++ src/main/java/com/giantdwarf/modules/study/StudyRepositoryExtensionImpl.java
@@ -1,6 +1,5 @@
 package com.giantdwarf.modules.study;
 
-import com.giantdwarf.modules.account.QAccount;
 import com.giantdwarf.modules.tag.QTag;
 import com.giantdwarf.modules.zone.QZone;
 import com.querydsl.core.QueryResults;
@@ -25,7 +24,6 @@ public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport impl
                 .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword)))
                 .leftJoin(study.tags, QTag.tag).fetchJoin()
                 .leftJoin(study.zones, QZone.zone).fetchJoin()
-                .leftJoin(study.members, QAccount.account).fetchJoin()
                 .distinct();
         //querydsl projection으로 더 성능최적화를 할 수 있다.
         JPQLQuery<Study> pageableQuery = getQuerydsl().applyPagination(pageable, query);
