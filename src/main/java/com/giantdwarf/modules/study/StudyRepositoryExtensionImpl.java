ba4650cffdc588d4399bcdf8a2fcfbcfc21db641
seungin yang <rhfpdk92@naver.com>
2020-08-22 23:58:29 +0900

    solve N+1 problem with petch join

--- src/main/java/com/giantdwarf/modules/study/StudyRepositoryExtensionImpl.java
+++ src/main/java/com/giantdwarf/modules/study/StudyRepositoryExtensionImpl.java
@@ -1,5 +1,8 @@
 package com.giantdwarf.modules.study;
 
+import com.giantdwarf.modules.account.QAccount;
+import com.giantdwarf.modules.tag.QTag;
+import com.giantdwarf.modules.zone.QZone;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
@@ -17,7 +20,12 @@ public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport impl
         JPQLQuery<Study> query = from(study).where(study.published.isTrue()
                 .and(study.title.containsIgnoreCase(keyword))
                 .or(study.tags.any().title.containsIgnoreCase(keyword))
-                .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword)));
+                .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword)))
+                .leftJoin(study.tags, QTag.tag).fetchJoin()
+                .leftJoin(study.zones, QZone.zone).fetchJoin()
+                .leftJoin(study.members, QAccount.account).fetchJoin()
+                .distinct();
+        //querydsl projection으로 더 성능최적화를 할 수 있다.
         return query.fetch();
     }
 }
